7febec116d56679e684494e3e604134630db4d09
Steven Espinoza <espisteven5@gmail.com>
2024-01-30 16:02:12 -0800

    added to package.json pipfile and added tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,24 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            gridTemplateRows: {
+                "[auto,auto,1fr]": "auto auto 1fr",
+            },
+            textShadow: {
+                default: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require("@tailwindcss/aspect-ratio"),
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+    ],
+};
