2fc36ae742daf8fe1b9620fa94be5ec343fac18e
Steven Espinoza <espisteven5@gmail.com>
2024-02-05 15:34:04 -0800

    auth updated and tested

--- server/config.py
+++ server/config.py
@@ -7,10 +7,10 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
@@ -20,9 +20,15 @@ load_dotenv()
 
 app = Flask(__name__, static_folder="./static", static_url_path="/static")
 
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "default_secret_key")
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_USE_SIGNER"] = True
+Session(app)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URI", "sqlite:///app.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 app.json.compact = False
 CORS(app)
 # Define metadata, instantiate db
@@ -37,7 +43,6 @@ ma.init_app(app)
 
 migrate = Migrate(app, db)
 db.init_app(app)
-jwt = JWTManager(app)
 bcrypt = Bcrypt(app)
 
 # Instantiate REST API
